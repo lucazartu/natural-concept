4d1cce8ec47054b87f0977ef9e4c82f6783a4c0c
JoaoSant0s <ricardoelric@gmail.com>
2017-11-27 03:35:39 -0300

    Adding ALLOWED_HOSTS in system, to run multiples services in same machine.

--- shopService/shopService/settings.py
+++ shopService/shopService/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '5gvu&on_j!5l7&(kalu1at7v-09suljh3xsh%yc4*14ymdsqo*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
